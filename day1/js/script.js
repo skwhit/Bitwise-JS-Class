085ba5a451cf977b4eec887bb7663a7e03fdf58d
skwhit <whitlowspencer@gmail.com>
2022-09-21 16:46:13 -0700

    added comments and console log to js

--- day1/js/script.js
+++ day1/js/script.js
@@ -1,3 +1,16 @@
 document.querySelector('button').addEventListener("click", () => {
     alert("Hello World");
 });
+
+//This is a single line comment.
+
+/* This is
+a muliple
+line comment */
+
+const firstName = 'Spencer';
+let favFood = 'Ramen'
+
+console.log('My name is ' + firstName);
+console.log('My favorite food is ' + favFood);
+
