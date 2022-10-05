188eac43a2d9d4e216aacbfd7288ee982a8beb38
skwhit <whitlowspencer@gmail.com>
2022-10-04 17:47:18 -0700

    made allowance for decimals and fractions with user input

--- day4/dailyAndTakeHome/js/main.js
+++ day4/dailyAndTakeHome/js/main.js
@@ -1,63 +1,62 @@
-// ------------ DAILY CHALLENGE ------------
-const h1 = document.querySelector('h1');
-const label = document.querySelector('label');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const h1 = document.querySelector("h1");
+const label = document.querySelector("label");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 let num = 0;
 let counter = 0;
 let numbers = [];
-let intOne;
-let intTwo;
 let result;
-label.innerHTML = "Please enter a number"
+label.innerHTML = "Please enter a number";
 
-input.addEventListener("keypress", function(event) {
+input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
     button.click();
   }
 });
 
-// Prompt the user for two different numbers and display which, if any, is larger
 button.addEventListener("click", () => {
-  num = input.value;
-  input.value=''
+  console.log(input.value);
+  if (/\//.test(input.value)) {
+    num = input.value.split("/");
+    num = Number(num[0], 10) / Number(num[1], 10);
+  } else if (/\,/.test(input.value)) {
+    num = input.value.split(",");
+    num = Number(num.join(""));
+  } else {
+    num = Number(input.value);
+  }
+  console.log(num);
+  input.value = "";
   input.focus();
-  
+
   if (counter < 2) {
-    if (/^[+-]{0,1}\d+$/.test(num)) {
+    if (!isNaN(num) && num != 0) {
       numbers.push(num);
-      counter ++;
-    } 
-    else {
+      counter++;
+    } else {
       alert("Your input is not a number, please enter a number");
     }
   }
-  
-  if (counter===2) {
-    // Convert the values to integers
-    // Store the two integers as variables
-    intOne = parseInt(numbers[0]);
-    intTwo = parseInt(numbers[1]);
 
-    // Compare the two numbers
-    if (intTwo === intOne) {
-      result = `${intTwo} is equal to ${intOne}`;
-    } else if (intTwo > intOne) {
-      result = `${intTwo} is larger than ${intOne}`;
+  if (counter === 2) {
+    if (numbers[1] === numbers[0]) {
+      result = `${numbers[1]} is equal to ${numbers[0]}`;
+    } else if (numbers[1] > numbers[0]) {
+      result = `${numbers[1]} is larger than ${numbers[0]}`;
     } else {
-      result = `${intOne} is larger than ${intTwo}`;
+      result = `${numbers[0]} is larger than ${numbers[1]}`;
     }
-    console.log(result)
+    console.log(result);
     h1.innerHTML = result;
 
     counter = 0;
     numbers = [];
   }
-  if (counter===1) {
-    label.innerHTML = "Please enter a second number"
+  if (counter === 1) {
+    label.innerHTML = "Please enter a second number";
   }
-  if (counter===0) {
-    label.innerHTML = "Please enter a number"
+  if (counter === 0) {
+    label.innerHTML = "Please enter a number";
   }
 });
