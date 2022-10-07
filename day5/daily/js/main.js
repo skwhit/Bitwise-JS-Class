5e0c2f81f26262325892ea689174a6682c3962ff
skwhit <whitlowspencer@gmail.com>
2022-10-06 18:50:08 -0700

    modified day 5 and initial commit for day 6

--- day5/daily/js/main.js
+++ day5/daily/js/main.js
@@ -1,19 +1,21 @@
-const h1 = createh1();
+const h1Text1 = createh1();
 let input1 = createTextInput();
 const button1 = createButton();
-const h2 = createh2();
+const h2Text1 = createh2();
 let num;
-document.querySelector('input').focus();
+input1.focus();
 
 function createh1() {
     const h1 = document.createElement('h1');
     h1.textContent = "Please enter a whole number to determine whether it's odd or even:";
     document.body.appendChild(h1);
+    return h1;
 }
 
 function createTextInput() {
     const input = document.createElement('input');
     document.body.appendChild(input);
+    return input;
 }
 
 function createButton() {
@@ -21,9 +23,10 @@ function createButton() {
     button.textContent = 'Submit (Press Enter)';
     document.body.appendChild(button);
     button.addEventListener('click', oddOrEven);
+    return button;
 }
 
-document.querySelector('input').addEventListener("keypress", function (event) {
+input1.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
       event.preventDefault();
       document.querySelector('button').click();
@@ -34,20 +37,21 @@ function createh2() {
     const h2 = document.createElement('h2');
     h2.textContent = '';
     document.body.appendChild(h2);
+    return h2;
 }
 
 function oddOrEven() {
-    num = Number(document.querySelector('input').value);
-    document.querySelector('input').value = "";
-    document.querySelector('input').focus();
+    num = Number(input1.value);
+    input1.value = "";
+    input1.focus();
     console.log(num);
     if (num %2===0) {
         result = `${num} is an even number`
-        document.querySelector('h2').textContent = result;
+        h2Text1.textContent = result;
     }
     else if (num%2===1||num%2===-1) {
         result = `${num} is an odd number`
-        document.querySelector('h2').textContent = result;
+        h2Text1.textContent = result;
     }
     else {
         alert('This input is not a whole number. Please enter a whole number.')
